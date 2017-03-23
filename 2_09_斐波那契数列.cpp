cc3d3578b8ac3ed916a2b0f3ac065c329734e52d
He Zhang (Rudy) <425220727@qq.com>
2017-03-23 14:45:53 +0800

    Create 2_09_斐波那契数列.cpp

---
+++ 2_09_斐波那契数列.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+
+// method 1 
+// 利用特征方程 f(n) = f(n-1) + f(n-2)
+// x^2 = x + 1
+// x1 = (1+sqrt(5))/2
+// x2 = (1-sqrt(5))/2
+// f(n) = a*x1 + b*x2, a,b利用初始条件求出 
+int fib_1() {
+
+}
